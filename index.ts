b4eae763d0937e303729248dc3045e03bde9dd99
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-31 18:44:38 -0300

    improvement: using dotenv/config for more space

--- index.ts
+++ index.ts
@@ -1,17 +1,15 @@
-import dotenv from "dotenv";
-import Client from "./src/Client";
-import { readFileSync } from "fs";
-
-dotenv.config();
+import 'dotenv/config';
+import Client from './src/Client';
+import { readFileSync } from 'fs';
 
 const client = new Client();
 
-console.log(readFileSync("Mimitsu.txt", "utf8").toString());
+console.log(readFileSync('Mimitsu.txt', 'utf8').toString());
 
 client
-  .on("disconnect", () => console.log("Connection lost"))
-  .on("reconnect", () => console.log("Attempting to reconnect"))
-  .on("error", (err: Error) => console.error(err))
-  .on("warn", info => console.log(info));
+  .on('disconnect', () => console.log('Connection lost'))
+  .on('reconnect', () => console.log('Attempting to reconnect'))
+  .on('error', (err: Error) => console.error(err))
+  .on('warn', info => console.log(info));
 
 client.start();
