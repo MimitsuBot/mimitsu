45736833d90511708fe302022e8efec3d57d14c8
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-04-10 23:05:11 -0300

    stop

--- src/utils/EndpointUtils.js
+++ src/utils/EndpointUtils.js
@@ -1,129 +1,92 @@
-import fetch from 'node-fetch';
-import jwt from 'jsonwebtoken';
+const fetch = require('node-fetch')
+const jwt = require('jsonwebtoken')
 
-const BASE_API_URL = 'https://discordapp.com/api';
+const API_URL = 'https://discordapp.com/api'
 
 module.exports = class EndpointUtils {
-  static authenticate({ client }, adminOnly = false, fetchGuilds = false) {
+  static authenticate ({ client }, adminOnly = false, fetchGuilds = false) {
     return async (req, res, next) => {
-      const authorization = req.get('Authorization');
-
-      console.log(`Authorization: ${authorization}`)
-
+      const authorization = req.get('Authorization')
       if (authorization) {
-        const [identifier, token] = authorization.split(' ');
-
-        console.log(`Identifier: ${identifier} \nToken: ${token}`)
-
-        if (!identifier || !token)
-          return res.status(400).json({ success: false });
+        const [ identifier, token ] = authorization.split(' ')
+        if (!identifier || !token) return res.status(400).json({ ok: false })
 
         switch (identifier) {
           case 'User':
             if (!adminOnly) {
               try {
-                const { accessToken } = jwt.verify(
-                  token,
-                  process.env.JWT_SECRET
-                );
-
-                console.log(`accessToken: ${accessToken}`)
-
-                req.user = await this.fetchUser(accessToken);
-                if (fetchGuilds)
-                  req.guilds = await this.fetchGuilds(client, accessToken);
-                return next();
+                const { accessToken } = jwt.verify(token, process.env.JWT_SECRET)
+                req.user = await this._fetchUser(client, accessToken)
+                if (fetchGuilds) req.guilds = await this._fetchGuilds(client, accessToken)
+                return next()
               } catch (e) {
-                return res.status(401).json({ success: false });
+                return res.status(401).json({ ok: false })
               }
             }
-            break;
+            break
           case 'Admin':
             if (token === process.env.ADMIN_TOKEN) {
-              req.isAdmin = true;
-              return next();
+              req.isAdmin = true
+              return next()
             }
         }
-        return res.status(401).json({ success: false });
+        return res.status(401).json({ ok: false })
       }
-      return res.status(400).json({ success: false });
-    };
-  }
-
-  static async fetchUser(accessToken) {
-    return this._request('/users/@me', accessToken);
-  }
-
-  static async fetchGuilds(client, accessToken) {
-    return this._request('/users/@me/guilds', accessToken).then(gs => {
-      gs.map(g => {
-        g.common = client.guilds.cache.has(g.id);
-        return g;
-      });
-    });
-  }
-
-  static async _request(endpoint, accessToken) {
-    if (!accessToken) {
-      throw new Error('You need to provide a valid token.');
+      return res.status(400).json({ ok: false })
     }
-
-    return fetch(`${BASE_API_URL}${endpoint}`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    }).then(res => (res.success ? res.json() : Promise.reject(res)));
   }
 
-  static async handleUser({ client }) {
+  static _fetchUser (client, token) {
+    return this._requestDiscord('/users/@me', token)
+  }
+
+  static async _fetchGuilds (client, token) {
+    return this._requestDiscord('/users/@me/guilds', token).then(gs => gs.map(g => {
+      g.common = client.guilds.has(g.id)
+      return g
+    }))
+  }
+
+  static _requestDiscord (endpoint, token) {
+    if (!token) throw new Error('INVALID_TOKEN')
+
+    return fetch(`${API_URL}${endpoint}`, {
+      headers: { 'Authorization': `Bearer ${token}` }
+    }).then(res => res.ok ? res.json() : Promise.reject(res))
+  }
+
+  static handleUser ({ client }) {
     return (req, res, next) => {
-      let { userId } = req.params;
-
-      if (!userId) {
-        return res
-          .status(401)
-          .json({ error: 'You must provide a valid userID' });
-      }
-
-      switch (userId) {
-        case '@me':
-          userId = req.isAdmin ? client.user.id : req.user.id;
-          break;
-        default:
-          if (!req.isAdmin && userId !== req.user.id) {
-            return res.status(403).json({ error: 'Missing permissions' });
-          }
-      }
-
-      req.userId = userId;
-
-      return next();
-    };
-  }
-
-  static async handleGuild({ client }, permissions = 'MANAGE_GUILD') {
-    return async (req, res, next) => {
-      const { guildId } = req.params;
-
-      if (!guildId) {
-        return res
-          .status(401)
-          .json({ error: 'You must provide a valid guildID' });
-      }
-
-      const guild = client.guilds.cache.get(guildId);
-
-      if (!guild) return res.status(400).json({ success: false });
-
-      if (!req.isAdmin) {
-        const member = await guild.members.cahe.fetch(req.user.id);
-
-        if (!member || (permissions && !member.hasPermission(permissions))) {
-          return res.status(403).json({ error: 'Missing permissions' });
+      let id = req.params.userId
+      if (id) {
+        switch (id) {
+          case '@me':
+            id = req.isAdmin ? client.user.id : req.user.id
+            break
+          default:
+            if (!req.isAdmin && id !== req.user.id) return res.status(403).json({ error: 'Missing permissions!' })
         }
+        req.userId = id
+        return next()
       }
-
-      req.guildId = guildId;
-
-      return next();
-    };
+      return res.status(401).json({ error: 'Invalid user id!' })
+    }
   }
-};
+
+  static handleGuild ({ client }, permissions = 'MANAGE_GUILD') {
+    return async (req, res, next) => {
+      let id = req.params.guildId
+      if (id) {
+        const guild = client.guilds.get(id)
+        if (!guild) return res.status(400).json({ ok: false })
+        if (!req.isAdmin) {
+          const member = await guild.fetchMember(req.user.id)
+          if (!member || (permissions && !member.hasPermission(permissions))) return res.status(403).json({ error: 'Missing permissions!' })
+        }
+        req.guildId = id
+        return next()
+      }
+      return res.status(401).json({ error: 'Invalid guild id!' })
+    }
+  }
+}
