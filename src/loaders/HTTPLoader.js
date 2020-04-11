f3bd3843fd685576b64cbbaf4be1db94f8deb937
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-04-11 13:37:50 -0300

    Update HTTPLoader.js

--- src/loaders/HTTPLoader.js
+++ src/loaders/HTTPLoader.js
@@ -27,7 +27,6 @@ module.exports = class HTTPLoader {
   initializeHTTPServer(port = process.env.PORT || 3333) {
     this.app = express();
 
-    this.app.use(cors({origin: 'http://localhost:8080'}))
     this.app.use(express.json());
 
     this.app.use(
